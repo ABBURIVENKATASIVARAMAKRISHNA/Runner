15e3b521dabae6b7ec0d66e8cb24b05b465fe289
ABBURIVENKATASIVARAMAKRISHNA <avsrk2002@gmial.com>
2023-12-14 15:55:01 +0530

    added into dev

--- src/com/jsp/Runner.java
+++ src/com/jsp/Runner.java
@@ -5,6 +5,13 @@ public class Runner {
 	private int id;
 	private String name;
 	private String address;
+	private String password;
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	public int getId() {
 		return id;
 	}
